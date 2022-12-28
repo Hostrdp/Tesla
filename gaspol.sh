042fff65b29759400726e577f071e65d6a3676a7
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2022-12-28 21:24:34 +0700

    Update gaspol.sh

--- gaspol.sh
+++ gaspol.sh
@@ -1,4 +1,6 @@
-#!/bin/sh.
+#!/bin/bash
 
-wget -O- --no-check-certificate https://s.id/win2k16dc | gunzip | dd of=/dev/sda
-reboot
+apt-get update
+apt-get install -y xz-utils openssl gawk file
+
+sudo wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && sudo bash InstallNET.sh -dd 'http://139.99.78.84/Windows2k16.gz'
