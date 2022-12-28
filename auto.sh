319a7aa59e75709d8035510f1233a7547b4248c9
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2022-12-29 04:09:03 +0700

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -5,4 +5,4 @@
 apt-get update
 apt-get install -y xz-utils openssl gawk file
 
-sudo wget --no-check-certificate -qO InstallNET.sh 'https://
+sudo wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/Hostrdp/Tesla/main/InstallNET.sh'
