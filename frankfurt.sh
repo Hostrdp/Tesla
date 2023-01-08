7946333a1cc5f8d6a3d98b317ff5dc5c5d66a4a0
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-08 16:44:59 +0700

    Update frankfurt.sh

--- frankfurt.sh
+++ frankfurt.sh
@@ -3,4 +3,5 @@
 apt-get update
 
 wget -O- --no-check-certificate 'http://161.35.216.161/Win2k12dc.gz' | gunzip | dd of=/dev/sda
-shutdown -r
+echo 1 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
