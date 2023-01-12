393131c8177b5d116083b3eddb39ab9d0cbc831b
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-13 00:00:03 +0700

    Update and rename frankfurt.sh to sda.sh

--- frankfurt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt-get update
-
-wget -O- --no-check-certificate 'http://161.35.216.161/Win2k12dc.gz' | gunzip | dd of=/dev/sda
-echo 1 > /proc/sys/kernel/sysrq
-echo b > /proc/sysrq-trigger
