63cb129d88d15c0e8d21f12f723cd49c0fd23fd6
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-14 16:05:42 +0700

    Update sda.sh

--- sda.sh
+++ sda.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-apt install screen -y
 apt-get update
-screen -t win
 
 wget -O- --no-check-certificate 'https://s.id/Win2k12dc' | gunzip | dd of=/dev/sda
 echo 1 > /proc/sys/kernel/sysrq
