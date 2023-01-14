0711426f056a0b8b0183847d705011acd1fb859e
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-15 01:20:14 +0700

    Update vda.sh

--- vda.sh
+++ vda.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 apt-get update
+interface=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d':')
 
-wget -O- --no-check-certificate Win2k12.gz 'https://s.id/Win2k12dc' | gunzip | dd of=/dev/vda
+wget -O- --no-check-certificate 'https://s.id/Win2k12dc' | gunzip | dd of=/dev/vda
+ip link set $interface
 echo 1 > /proc/sys/kernel/sysrq
 echo b > /proc/sysrq-trigger
