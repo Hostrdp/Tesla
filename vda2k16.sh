337125c15d22e7deacf0beac91900109274d8370
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-14 14:08:09 +0700

    Update vda2k16.sh

--- vda2k16.sh
+++ vda2k16.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 
-wget -O- --no-check-certificate Win2k16.gz 'https://s.id/Win2k16dc' | gunzip | dd of=/dev/sda
+wget -O- --no-check-certificate Win2k16.gz 'https://s.id/Win2k16dc' | gunzip | dd of=/dev/vda
 echo 1 > /proc/sys/kernel/sysrq
 echo b > /proc/sysrq-trigger
