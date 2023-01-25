f8984847db3c547c809b7de08edd4cf757c213b0
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-25 16:17:42 +0700

    Update sda.sh

--- sda.sh
+++ sda.sh
@@ -2,6 +2,7 @@
 
 apt-get update
 
-dd if=w10.gz bs=1M status=progress | gunzip -dc | dd of=/dev/sda
+wget --no-check-certificate -O w12.gz 'https://s.id/Win2k12dc'
+dd if=w12.gz bs=1M status=progress | gunzip -dc | dd of=/dev/sda
 echo 1 > /proc/sys/kernel/sysrq
 echo b > /proc/sysrq-trigger
