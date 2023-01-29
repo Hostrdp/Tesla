02eb822f909d15f0f74c50cc9fd031e194d1f39e
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-01-29 23:56:05 +0700

    Update sda.sh

--- sda.sh
+++ sda.sh
@@ -3,6 +3,7 @@
 apt install -y screen
 apt-get update
 screen -S tesla
+sleep 2
 wget -O w12.gz 'https://s.id/Win2k12dc'
 gunzip -dc w12.gz | dd of=/dev/sda bs=4M status=progress
 echo 1 > /proc/sys/kernel/sysrq
