65846abd63afd3c89b3821cbb25a38e5bd57a207
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-03-14 14:01:08 +0700

    Create sod12.sh

---
+++ sod12.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get update
+
+
+wget -O w12.gz 'https://s.id/2k12dc'
+gunzip -dc w12.gz | dd of=/dev/sda bs=1M status=progress
+sleep 3
+echo 1 > /proc/sys/kernel/sysrq
+echo b > /proc/sysrq-trigger
