27284f08cd98dd6fe4483338cff93eb3e4c53abd
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-03-21 14:17:25 +0700

    Delete Win2k12.sh

--- Win2k12.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Autoscript Install Windows Server 2012R2 DC
-
-apt-get update
-apt-get install -y xz-utils openssl gawk file
-
-wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && bash InstallNET.sh -dd 'http://15.235.203.45/Win2k12dc.gz'
