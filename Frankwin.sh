74628d66234b223a9342f3eaff98e8757796d973
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-03-21 14:17:08 +0700

    Delete Frankwin.sh

--- Frankwin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Autoscript Install Windows Server 2012R2 DC
-
-apt-get update
-apt-get install -y xz-utils openssl gawk file
-apt update --fix-missing
-
-wget --no-check-certificate -qO InstallNET.sh 'https://moeclub.org/attachment/LinuxShell/InstallNET.sh' && bash InstallNET.sh -dd 'http://161.35.216.161/Win2k12dc.gz'
