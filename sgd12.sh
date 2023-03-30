fcaa0fdc135ff547f88412361de30eecdd6eab06
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-03-30 19:08:34 +0700

    Update sgd12.sh

--- sgd12.sh
+++ sgd12.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+apt install -y xz-utils file gawk openssl wget curl
 apt-get update
 
 
-wget -O w12.gz 'https://s.id/Win2k12dc'
-gunzip -dc w12.gz | dd of=/dev/sda bs=1M status=progress
+wget -O w12.xz 'https://s.id/bintang12'
+xz -dc w12.xz | dd of=/dev/sda bs=1M status=progress
 sleep 3
 echo 1 > /proc/sys/kernel/sysrq
 echo b > /proc/sysrq-trigger
