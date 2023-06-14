83fb141ca25fe24585c3f6d7b5bc17ab16739613
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-06-14 13:27:59 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -226,8 +226,9 @@ function Start() {
   echo "  15)Template Akuma 100"
   echo "  16)Win 10 LTSC Teddysun"
   echo "  17)Win 11 Pro Teddysun"
-  echo "  18)Windows Server 2022 DC"
-  echo "  19)Akuma 50 v2"
+  echo "  18)Windows Server 2019 DC"
+  echo "  19)Windows Server 2022 DC"
+  echo "  20)Akuma 50 v2"
   echo "  0)Exit"
   echo -ne "\nYour option: "
   read N
@@ -258,8 +259,9 @@ function Start() {
     15) bash /tmp/InstallNET.sh $NETSTR -dd 'https://win.akumavm.com/akuma100.gz' $DMIRROR ;;
     16) bash /tmp/InstallNET.sh $NETSTR -dd 'https://dl.lamp.sh/vhd/en-us_windows10_ltsc.xz' $DMIRROR ;;
     17) bash /tmp/InstallNET.sh $NETSTR -dd 'https://dl.lamp.sh/vhd/en-us_windows11_22h2.xz' $DMIRROR ;;
-    18) bash /tmp/InstallNET.sh $NETSTR -dd 'https://dl.lamp.sh/vhd/en-us_win2022.xz' $DMIRROR ;;
-    19) bash /tmp/InstallNET.sh $NETSTR -dd 'https://win.akumavm.com/akuma50v2.xz' $DMIRROR ;;
+    18) bash /tmp/InstallNET.sh $NETSTR -dd 'https://dl.lamp.sh/vhd/en_win2019.xz' $DMIRROR ;;
+    19) bash /tmp/InstallNET.sh $NETSTR -dd 'https://dl.lamp.sh/vhd/en-us_win2022.xz' $DMIRROR ;;
+    20) bash /tmp/InstallNET.sh $NETSTR -dd 'https://win.akumavm.com/akuma50v2.xz' $DMIRROR ;;
     0) exit 0;;
     *) echo "Wrong input!"; exit 1;;
   esac
