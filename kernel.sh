5659fc72b4b05da835a8ba2e8c501fb98e3d6d04
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-07-18 02:56:23 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -221,6 +221,7 @@ function Start() {
   echo "  10)Windows Server 2022 DC Teddysun"
   echo "  11)Akuma 50 v2"
   echo "  12)Tesla Loops"
+  echo "  12)Tesla PRO Login"
   echo "  99) Custom image"
   echo "  0)Exit"
   echo -ne "\nYour option: "
@@ -238,6 +239,7 @@ function Start() {
     10) bash /tmp/InstallNET.sh $NETSTR -dd 'https://dl.lamp.sh/vhd/en-us_win2022.xz' $DMIRROR ;;
     11) bash /tmp/InstallNET.sh $NETSTR -dd 'https://win.akumavm.com/akuma50v2.xz' $DMIRROR ;;
     12) bash /tmp/InstallNET.sh $NETSTR -dd 'https://win.akumavm.com/teslaloops.gz' $DMIRROR ;;
+    13) bash /tmp/InstallNET.sh $NETSTR -dd 'https://win.akumavm.com/teslapro.gz' $DMIRROR ;;
     99)
       echo -e "\n"
       read -r -p "Custom image URL: " imgURL
