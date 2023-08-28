195f19c763973fa20307f734025e69dde21b9092
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-08-28 23:07:55 +0700

    Delete sda2k16.sh

--- sda2k16.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update
-
-wget -O w16.gz 'https://s.id/Win2k16dc'
-gunzip -dc w16.gz | dd of=/dev/sda bs=1M status=progress
-
-echo 1 > /proc/sys/kernel/sysrq
-echo b > /proc/sysrq-trigger
