b78d54f47ab9e88df4c0506653d6d6f3d0a6b276
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-09-12 02:34:41 +0700

    Update ipv6.sh

--- ipv6.sh
+++ ipv6.sh
@@ -14,7 +14,7 @@ gen64() {
 install_3proxy() {
     echo "installing 3proxy"
     URL="https://raw.githubusercontent.com/quayvlog/quayvlog/main/3proxy-3proxy-0.8.6.tar.gz"
-    wget -qO- $URL | bsdtar -xvf-
+    wget -qO- $URL | tar -xvzf
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/bin
