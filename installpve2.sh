aebb0483318a464bd275b679218ce70da42424d4
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2023-12-08 09:58:36 +0700

    Update installpve2.sh

--- installpve2.sh
+++ installpve2.sh
@@ -4,6 +4,6 @@ apt install proxmox-ve postfix open-iscsi -y
 apt remove linux-image-amd64 'linux-image-5.10*' -y
 update-grub
 apt remove os-prober -y
-
+apt-get install -y iptables iptables-persistent
 bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
 bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/misc/post-pve-install.sh)" -y
