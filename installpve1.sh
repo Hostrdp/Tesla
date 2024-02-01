728d3757b371e22637d45d9d25fd4825a6191913
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2024-02-01 23:47:46 +0700

    Update installpve1.sh

--- installpve1.sh
+++ installpve1.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 apt update && apt upgrade -y
-apt install -y wget curl nano screen ftp
+
+echo "Install and Setup pre-run script"
+apt install -y wget curl nano screen ftp linux-cpupower chrony
+systemctl enable chrony
+
+echo "Configures CPU freqs"
+cpupower frequency-set --governor performance
+cpupower idle-set --disable-by-latency 0
+
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+
 # Verify
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 apt update && apt full-upgrade -y
