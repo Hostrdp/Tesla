cf7754ac14e1e1e7298de4e67cec821005e8d8d9
Hostrdp <119750769+Hostrdp@users.noreply.github.com>
2024-02-01 23:37:30 +0700

    Update installpve2.sh

--- installpve2.sh
+++ installpve2.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo "Installing PVE and dependecies"
 apt install proxmox-ve postfix open-iscsi -y
 apt remove linux-image-amd64 'linux-image-5.10*' -y
 update-grub
 apt remove os-prober -y
+
+echo "Install IP Tables"
 apt-get install -y iptables iptables-persistent
+
+echo "Setup Fail2Ban for Security"
+bash <(wget -qO- --no-check-certificate https://raw.githubusercontent.com/Hostrdp/Tesla/main/secure.sh)
+
+echo "Install Dark Themes for PVE"
 bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
+
+echo "Running Post Install PVE script"
 bash -c "$(wget -qLO - https://github.com/tteck/Proxmox/raw/main/misc/post-pve-install.sh)" -y
